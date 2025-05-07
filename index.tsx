8f5ad97c4c0515513912cf3f89202343058f74ff
Nikoyaka9dd <minikkosumikko@gmail.com>
2025-05-07 11:33:09 +0900

    twitterのurl変更

--- index.tsx
+++ index.tsx
@@ -78,7 +78,7 @@ export default function Portfolio() {
       </section>
 
       <div className="sns-box">
-        <a href="https://x.com/jibathis2583" className="btn-sns btn-twitter">
+        <a href="https://x.com/nikoyaka_com" className="btn-sns btn-twitter">
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
             <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
           </svg>
