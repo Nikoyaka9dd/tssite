ab07b560c2cce0825cf8824f43b4877b6b136e95
Nikoyaka9dd <minikkosumikko@gmail.com>
2025-05-07 12:03:28 +0900

    データベース接続すっぞ

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,12 @@ import { PrismaClient } from "@prisma/client"
 // PrismaClientのグローバルインスタンスを作成
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// 開発環境でのホットリロード時に複数のPrismaClientインスタンスが作成されるのを防ぐ
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
